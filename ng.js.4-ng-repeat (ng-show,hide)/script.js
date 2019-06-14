c91c2ceecb4a5cacccafbc31b26395d4e8ca1050
nakulGoyal19 <nin25ja02@gmail.com>
2019-06-14 12:42:02 +0530

    repeat

---
+++ ng.js.4-ng-repeat (ng-show,hide)/script.js
@@ -0,0 +1,23 @@
+// Code goes here
+(function(){
+  var app=angular.module("firstModule",[]);
+  var mainController=function($scope,$http)
+  {
+    var onUserComplete=function(response){
+      $scope.user=response.data;
+      $http.get($scope.user.repos_url).then(onRepos,onError);
+    };
+    var onRepos=function(response){
+      $scope.repos=response.data;
+    };
+    var onError=function(reason)
+    {
+      $scope.error="Could not fetch the error"
+    };
+    $scope.message="Angular-js Controllers and Module";
+    $scope.search=function(username){
+    $http.get("https://api.github.com/users/"+username).then(onUserComplete,onError);
+    };
+  };
+  app.controller("mainController",mainController);
+}());
